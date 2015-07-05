4d74afdb797ce543a1e47eaaa0ec4d0979eb740f
Vedran Čačić <vedgar+github@gmail.com>
2015-07-05 20:55:15 +0200

    P vs NP. See Hettinger's videos.
    This was just horrible. Race conditions, PY2-only syntax, forgetting information in exceptions, repeated flow control, code inconsistencies, typos... It's still a horror, but at least a small step in the right direction.
    
    And why are there no tests? How do I know whether my code works? :-o
    Of course, in current situation, you'd be foolish to accept my code. It only runs on Python interpreter in my head. :-) But in the future, I hope the codebase will look more sensible and I'll be able to help you much more.

--- checkio_cli/getters.py
+++ checkio_cli/getters.py
@@ -3,78 +3,72 @@ import sys
 import git
 import re
 import shutil
-from distutils.dir_util import copy_tree
 import logging
+from distutils.dir_util import copy_tree
 
 from checkio_docker.parser import MissionFilesCompiler
-from checkio_cli.folder import Folder
 from checkio_docker.client import DockerClient
+from checkio_cli.folder import Folder
 from checkio_cli.config import settings
-
-PY3 = sys.version_info[0] == 3
-if PY3:
-    raw_input = input
-
 
 RE_REPO_BRANCH = re.compile('(.+?)\@([\w\-\_]+)$')
 
 
-class GetterExeption(Exception):
+class GetterException(Exception):
     pass
 
+if "that typo is not yet fixed in the whole codebase":
+    GetterExeption = GetterException
 
-class TemplateWasntFound(GetterExeption):
+
+class TemplateWasntFound(GetterException):
     def __init__(self, template, folders):
         self.template = template
         self.folders = folders
 
     def __str__(self):
-        return 'Template "{}"" wasn\'t found in folder(s) "{}"'.format(
-            self.template, '","'.join(self.folders)
-        )
+        folders = ','.join(map(repr, self.folders))
+        return "Template {!r} wasn't found in folder(s) {}".format(self.template, folders)
 
 
-class MissionFolderExistsAlready(GetterExeption):
+class MissionFolderExistsAlready(GetterException):
     def __init__(self, folder):
         self.folder = folder
 
     def __str__(self):
-        return 'Mission folder "{}" exists already'.format(self.folder)
+        return 'Mission folder {!r} exists already'.format(self.folder)
+
+
+def remove_folder(folder):
+    import contextlib, shutil
+    with contextlib.suppress(FileNotFoundError):
+        shutil.rmtree(folder)
 
 
 def make_mission_from_template(mission, template, force_remove=False):
-    template_full_path = None
     for template_folder in settings.TEMPLATES_FOLDERS:
         template_full_path = os.path.join(template_folder, template)
         if os.path.exists(template_full_path):
             break
-        else:
-            template_full_path = None
-
-    if template_full_path is None:
+    else:
         raise TemplateWasntFound(template, settings.TEMPLATES_FOLDERS)
 
     folder = Folder(mission)
     mission_folder = folder.mission_folder()
-    if os.path.exists(mission_folder):
-        if force_remove:
-            shutil.rmtree(mission_folder)
-        else:
-            raise MissionFolderExistsAlready(mission_folder)
+    if force_remove:
+        shutil.rmtree(mission_folder)
+    elif os.path.exists(mission_folder):
+        raise MissionFolderExistsAlready(mission_folder)
 
     os.mkdir(mission_folder)
 
-    from distutils.dir_util import copy_tree
     copy_tree(os.path.join(template_full_path, 'source'), mission_folder)
 
-    GG = {}
-    exec(open(os.path.join(template_full_path, 'run.py')).read()) in GG
-    GG['run'](mission)
+    namespace = {}
+    exec(open(os.path.join(template_full_path, 'run.py')).read(), namespace)
+    namespace['run'](mission)
 
-    folder.mission_config_write({
-        'type': 'local',
-        'url': mission_folder
-    })
+    folder.mission_config_write(dict(type='local', url=mission_folder))
 
 
 def mission_git_init(mission, original_url):
@@ -96,14 +90,11 @@ def mission_git_init(mission, original_url):
     origin.push(repo.refs)
     origin.fetch()
     repo.create_head('master', origin.refs.master).set_tracking_branch(origin.refs.master)
-    folder.mission_config_write({
-        'type': 'git',
-        'url': original_url
-    })
+    folder.mission_config_write(dict(type='git', url=original_url))
 
 
 def mission_git_getter(url, slug):
-    # TODO: checkout into mission solder
+    # TODO: checkout into mission folder
     # compile it
     # build docker
     # prepare cli interface
@@ -114,30 +105,23 @@ def mission_git_getter(url, slug):
     logging.info('from %s to %s', url, destination_path)
 
     if os.path.exists(destination_path):
-        answer = raw_input('Folder {} exists already.'
+        answer = input('Folder {} exists already.'
                            ' Do you want to overwite it? [y]/n :'.format(destination_path))
-        if answer is '' or answer.lower().startswith('y'):
+        if not answer or answer.lower().startswith('y'):
             shutil.rmtree(destination_path)
         else:
             return
 
-    re_ret = re.search(RE_REPO_BRANCH, url)
-    if re_ret:
-        checkout_url, branch = re_ret.groups()
-    else:
-        checkout_url = url
-        branch = 'master'
+    try:
+        checkout_url, branch = re.search(RE_REPO_BRANCH, url).groups()
+    except AttributeError:
+        checkout_url, branch = url, 'master'
 
     logging.debug('URL info: checkioout url:%s branch:%s', url, branch)
 
-    try:
-        git.Repo.clone_from(checkout_url, destination_path, branch=branch)
-    except git.GitCommandError as e:
-        raise Exception(u"{}, {}".format(e or '', e.stderr))
-    folder.mission_config_write({
-        'type': 'git',
-        'url': url
-    })
+    git.Repo.clone_from(checkout_url, destination_path, branch=branch)
+
+    folder.mission_config_write(dict(type='git', url=url))
     print('Prepare mission {} from {}'.format(slug, url))
 
 
@@ -149,18 +133,12 @@ def logging_sys(command):
 def rebuild_native(slug):
     folder = Folder(slug)
     logging.info('Building virtualenv in %s', folder.native_env_folder_path())
-    if os.path.exists(folder.native_env_folder_path()):
-        shutil.rmtree(folder.native_env_folder_path())
+    remove_folder(folder.native_env_folder_path())
 
     logging_sys("virtualenv --system-site-packages -p python3 " + folder.native_env_folder_path())
-    logging_sys("{pip3} install -r {requirements}".format(
-        pip3=folder.native_env_bin('pip3'),
-        requirements=folder.referee_requirements()
-    ))
-    logging_sys("{pip3} install -r {requirements}".format(
-        pip3=folder.native_env_bin('pip3'),
-        requirements=folder.interface_cli_requirements()
-    ))
+    for requirements in folder.referee_requirements(), folder.interface_cli_requirements():
+        logging_sys("{pip3} install -r {requirements}".format(
+            pip3=folder.native_env_bin('pip3'), requirements=requirements))
 
 
 def rebuild_mission(slug):
@@ -168,8 +146,7 @@ def rebuild_mission(slug):
     docker = DockerClient()
     verification_folder_path = folder.container_verification_folder_path()
     logging.info("Build docker image %s from %s", folder.image_name(), verification_folder_path)
-    if os.path.exists(verification_folder_path):
-        shutil.rmtree(verification_folder_path)
+    remove_folder(verification_folder_path)
 
     copy_tree(folder.verification_folder_path(), verification_folder_path)
     docker.build(name_image=folder.image_name(), path=verification_folder_path)
@@ -179,8 +156,7 @@ def recompile_mission(slug):
     folder = Folder(slug)
     compiled_path = folder.compiled_folder_path()
     logging.info("Relink folder to %s", compiled_path)
-    if os.path.exists(compiled_path):
-        shutil.rmtree(compiled_path)
+    remove_folder(compiled_path)
 
     mission_source = MissionFilesCompiler(compiled_path)
     mission_source.compile(source_path=folder.mission_folder(), use_link=True)
